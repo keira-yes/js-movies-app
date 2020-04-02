15326b8c2b8203c01cc725643f902d41afd232ab
Kirillova <keira.kirillova@gmail.com>
2020-04-02 17:46:11 +0300

    add popular items

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,20 @@
 const searchForm = document.getElementById('search-form'),
-  movies = document.getElementById('movies')
+  movies = document.getElementById('movies'),
   img_url = 'https://image.tmdb.org/t/p/w500/';
+
+// Show full info of each item
+function showInfo() {
+  console.log(this.dataset.type);
+}
+
+// Add click event to items
+function addEventMedia() {
+  const media = document.querySelectorAll('img[data-id]');
+  media.forEach(elem => {
+    elem.style.cursor = 'pointer';
+    elem.addEventListener('click', showInfo);
+  })
+}
 
 // Search request on fetch
 function searchApi(e) {
@@ -29,13 +43,20 @@ function searchApi(e) {
       }
 
       output.results.forEach(item => {
+        console.log(item);
         const name = item.name || item.title,
-          date = item.first_air_date || item.release_date,
-          img = item.backdrop_path === null ? 'img/no_image.png' : img_url + item.backdrop_path;
+          date = item.first_air_date || item.release_date || '',
+          img = item.backdrop_path ? img_url + (item.backdrop_path || item.profile_path) : 'img/no_image.png';
+        let dataInfo = '';
+
+        if (item.media_type !== 'person') {
+          dataInfo = `data-id=${item.id} data-type=${item.media_type}`
+        }
+
         inner += `
           <div class="col-12 col-md-6 col-xl-3">
             <div class="card mb-4">
-              <img src=${img} class="card-img-top" alt=${name}>
+              <img src="${img}" class="card-img-top" alt="${name}" ${dataInfo}>
               <div class="card-body">
                 <h5 class="card-title">${name}</h5>
                 <span>${date}</span>
@@ -46,6 +67,9 @@ function searchApi(e) {
       });
 
       movies.innerHTML = inner;
+
+      addEventMedia();
+
     })
     .catch(reason => {
       movies.innerText = 'Что-то пошло не так...';
@@ -54,3 +78,46 @@ function searchApi(e) {
 }
 
 searchForm.addEventListener('submit', searchApi);
+
+// Show popular items
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://api.themoviedb.org/3/trending/all/day?api_key=0e66e3cd5d8c014d6e406d8aba055a88&language=ru-RU')
+    .then(response => {
+      if (response.status !== 200) {
+        return Promise.reject(new Error(response.status));
+      }
+      return response.json();
+    })
+    .then(output => {
+      let inner = '<h2 class="text-info"></h2>';
+
+      output.results.forEach(item => {
+        console.log(item);
+        const name = item.name || item.title,
+          date = item.first_air_date || item.release_date || '',
+          img = item.backdrop_path ? img_url + (item.backdrop_path || item.profile_path) : 'img/no_image.png';
+        let dataInfo = `data-id=${item.id} data-type=${item.media_type}`;
+
+        inner += `
+          <div class="col-12 col-md-6 col-xl-3">
+            <div class="card mb-4">
+              <img src="${img}" class="card-img-top" alt="${name}" ${dataInfo}>
+              <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <span>${date}</span>
+              </div>
+            </div>
+          </div>          
+        `;
+      });
+
+      movies.innerHTML = inner;
+
+      addEventMedia();
+
+    })
+    .catch(reason => {
+      movies.innerText = 'Что-то пошло не так...';
+      console.error('error', reason);
+    });
+});
